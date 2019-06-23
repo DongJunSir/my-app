4c3b12f0b2203040bb111bafa31140cd457e3ee8
DongJunSir <e_dongjun55@163.com>
2019-06-23 23:29:30 +0800

    添加代码

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+// import Hello from './components/StatefulHello';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore } from 'redux';
+import { EnthusiasmAction } from './actions/index';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+
+const store = createStore<StoreState, EnthusiasmAction, any, any>(enthusiasm, {
+  enthusiasmLevel: 1, 
+  languageName: 'TypeScript',
+});
+
+import Hello from './containers/index'
+import { Provider } from 'react-redux';
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
